3a15e92cb88ac92697bdbaeefef5462ae74ad050
Guilherme <62113851+ComicalTrader@users.noreply.github.com>
2025-10-17 00:31:27 -0300

    Barra Expand /collapse

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 const Sidebar = () => {
+  const menuItems = ["Home", "Agenda", "Financeiro", "Clientes"];
+
   return (
-    <aside className="w-64 min-h-screen p-6 text-white bg-gray-800">
-      <h2 className="mb-6 text-2xl font-bold">BarberPro</h2>
-      <ul className="space-y-4">
-        <li className="cursor-pointer hover:text-gray-300">Home</li>
-        <li className="cursor-pointer hover:text-gray-300">Agenda</li>
-        <li className="cursor-pointer hover:text-gray-300">Financeiro</li>
-        <li className="cursor-pointer hover:text-gray-300">Clientes</li>
+    <aside
+      className="flex flex-col items-center w-16 h-screen p-4 text-white transition-all duration-300 ease-in-out bg-gray-800  hover:w-64 hover:items-start"
+    >
+      <h2 className="hidden mb-8 text-xl font-bold hover:block">BarberPro</h2>
+
+      <ul className="flex flex-col w-full gap-4">
+        {menuItems.map((item) => (
+          <li
+            key={item}
+            className="flex items-center w-full gap-4 px-2 py-2 rounded cursor-pointer hover:bg-gray-700"
+          >
+            <span className="material-icons">circle</span>
+            <span className="hidden hover:block">{item}</span>
+          </li>
+        ))}
       </ul>
     </aside>
   );
