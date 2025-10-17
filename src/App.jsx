751847295de863018fc80bbe84b6be0352cb2cfd
Guilherme <62113851+ComicalTrader@users.noreply.github.com>
2025-10-17 00:22:39 -0300

    Base funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from "react";
+import Sidebar from "./components/Sidebar.jsx";
+import Home from "./pages/Home.jsx";
 
 const App = () => {
   return (
     <div className="flex min-h-screen">
-      <aside className="w-64 p-6 text-white bg-gray-800">
-        <h2 className="mb-6 text-2xl font-bold">BarberPro</h2>
-        <ul className="space-y-3">
-          <li>Home</li>
-          <li>Agenda</li>
-          <li>Financeiro</li>
-          <li>Clientes</li>
-        </ul>
-      </aside>
-
-      <main className="flex-1 p-6 bg-red-500">
-        <h1 className="text-3xl font-bold text-white">Testando Tailwind!</h1>
-        <p className="mt-4 text-white">Se você vê isso vermelho, Tailwind está funcionando!</p>
+      <Sidebar />
+      <main className="flex-1 p-6 bg-gray-100">
+        <Home />
       </main>
     </div>
   );
